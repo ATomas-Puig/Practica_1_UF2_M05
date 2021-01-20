95cb179228263d75cb7e139c43bd555cc506eae9
Antonio <atomas@elpuig.xeill.net>
2021-01-20 17:00:25 +0100

    Commit from original repo to Branch 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! Fer");
-        System.out.println("Hello World! Antonio");
     }
 }
